a1c2b94afaa66e853f8db83116ec40a66f3019eb
/codsec <44205014+codsec@users.noreply.github.com>
2022-11-29 08:41:01 -0400

    Update InstallVcredist.cmd

--- MAS/All-In-One-Version/InstallVcredist.cmd
+++ MAS/All-In-One-Version/InstallVcredist.cmd
@@ -1,3 +1,34 @@
+REM --add the following to the top of your bat file--
+
+
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+
 @echo off
 @setlocal enableextensions
 mode con:cols=90 lines=30
@@ -23,9 +54,11 @@ C:\VC_redist.x86.exe /uninstall /passive /quiet /norestart
 timeout 2 > NUL
 
 C:\VC_redist.x64.exe /install /passive /quiet /norestart
+C:\VC_redist.x86.exe /install /passive /quiet /norestart
 timeout 2 > NUL
 
 del /f "C:\VC_redist.x64.exe"
+del /f "C:\VC_redist.x86.exe"
 timeout 2 > NUL
 cls
 
