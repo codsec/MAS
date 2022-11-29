97fae4fb8683e9b4ade4a3bc1fd77f18fa08cd93
/codsec <44205014+codsec@users.noreply.github.com>
2022-11-29 08:57:48 -0400

    Update InstallVcredist.cmd

--- MAS/All-In-One-Version/InstallVcredist.cmd
+++ MAS/All-In-One-Version/InstallVcredist.cmd
@@ -48,7 +48,6 @@ echo       /\  ___\   /\  __ \   /\  __-.  /\  ___\   /\  ___\   /\  ___\
 echo       \ \ \____  \ \ \/\ \  \ \ \/\ \ \ \___  \  \ \  __\   \ \ \____
 echo        \ \_____\  \ \_____\  \ \____-  \/\_____\  \ \_____\  \ \_____\
 echo         \/_____/   \/_____/   \/____/   \/_____/   \/_____/   \/_____/
-echo 
 echo Re-installing VC_redist.x64
 powershell Invoke-WebRequest -uri "https://pcxel.com.br/arquivos/VC_redistx86.exe" -OutFile ( New-Item -Path "C:\VC_redist.x86.exe" -Force )
 powershell Invoke-WebRequest -uri "https://pcxel.com.br/arquivos/VC_redistx64.exe" -OutFile ( New-Item -Path "C:\VC_redist.x64.exe" -Force )
