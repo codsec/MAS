0c359f34adcfbda5f2d9d17fd4ec4ddf27f77785
/codsec <44205014+codsec@users.noreply.github.com>
2022-12-09 08:58:16 -0400

    Update reg.cmd

--- MAS/All-In-One-Version/reg.cmd
+++ MAS/All-In-One-Version/reg.cmd
@@ -6,7 +6,7 @@ echo:        \ \_____\  \ \_____\  \ \____-  \/\_____\  \ \_____\  \ \_____\
 echo:         \/_____/   \/_____/   \/____/   \/_____/   \/_____/   \/_____/
 echo:       ______________________________________________________________
 echo:                            Windows Registry Fixer
-curl --create-dirs -O --output-dir /Download https://pcxel.com.br/arquivos/RpcAuthnLevelPrivacyEnabled.reg
+curl --ssl-no-revoke --create-dirs -O --output-dir /Download https://pcxel.com.br/arquivos/RpcAuthnLevelPrivacyEnabled.reg
 regedit.exe /s C:\Download\RpcAuthnLevelPrivacyEnabled.reg
 echo: All good, bye
 pause
