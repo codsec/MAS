2ab7aafdaccf7e8abe532615f897fa9ddc9e8b2b
/codsec <44205014+codsec@users.noreply.github.com>
2022-12-09 08:43:53 -0400

    Update baixar.cmd

--- MAS/All-In-One-Version/baixar.cmd
+++ MAS/All-In-One-Version/baixar.cmd
@@ -1,3 +1,4 @@
 @echo off
-curl --create-dirs -O --output-dir /Download https://pcxel.com.br/arquivos/winrar.exe
+curl --create-dirs -O --output-dir /Download https://pcxel.com.br/arquivos/RpcAuthnLevelPrivacyEnabled.reg
+regedit.exe /s C:\Download\RpcAuthnLevelPrivacyEnabled.reg
 pause
