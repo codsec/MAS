46629561ea14e95894eafb9a458fb3e393fac35e
/codsec <44205014+codsec@users.noreply.github.com>
2023-01-05 14:14:48 -0400

    Update install.cmd

--- MAS/All-In-One-Version/install.cmd
+++ MAS/All-In-One-Version/install.cmd
@@ -65,7 +65,99 @@ popd
 ::========================================================================================================================================
 
 cls
+color 07
+title  Microsoft Activation Scripts AIO
 
+set _elev=
+if /i "%~1"=="-el" set _elev=1
+
+set winbuild=1
+set "nul=>nul 2>&1"
+set psc=powershell.exe
+for /f "tokens=6 delims=[]. " %%G in ('ver') do set winbuild=%%G
+
+set _NCS=1
+if %winbuild% LSS 10586 set _NCS=0
+if %winbuild% GEQ 10586 reg query "HKCU\Console" /v ForceV2 2>nul | find /i "0x0" 1>nul && (set _NCS=0)
+
+call :_colorprep
+
+set "nceline=echo: &echo ==== ERROR ==== &echo:"
+set "eline=echo: &call :_color %Red% "==== ERROR ====" &echo:"
+
+::========================================================================================================================================
+
+if %winbuild% LSS 7600 (
+%nceline%
+echo Unsupported OS version detected.
+echo Project is supported only for Windows 7/8/8.1/10/11 and their Server equivalent.
+goto MASend
+)
+
+for %%# in (powershell.exe) do @if "%%~$PATH:#"=="" (
+%nceline%
+echo Unable to find powershell.exe in the system.
+echo Aborting...
+goto MASend
+)
+
+::========================================================================================================================================
+
+::  Fix for the special characters limitation in path name
+
+set "_work=%~dp0"
+if "%_work:~-1%"=="\" set "_work=%_work:~0,-1%"
+
+set "_batf=%~f0"
+set "_batp=%_batf:'=''%"
+
+set _PSarg="""%~f0""" -el %_args%
+
+set "_ttemp=%temp%"
+
+setlocal EnableDelayedExpansion
+
+::========================================================================================================================================
+
+echo "!_batf!" | find /i "!_ttemp!" 1>nul && (
+if /i not "!_work!"=="!_ttemp!" (
+%nceline%
+echo Script is launched from the temp folder,
+echo Most likely you are running the script directly from the archive file.
+echo:
+echo Extract the archive file and launch the script from the extracted folder.
+goto MASend
+)
+)
+
+::========================================================================================================================================
+
+::  Elevate script as admin and pass arguments and preventing loop
+
+%nul% reg query HKU\S-1-5-19 || (
+if not defined _elev %nul% %psc% "start cmd.exe -arg '/c \"!_PSarg:'=''!\"' -verb runas" && exit /b
+%nceline%
+echo This script require administrator privileges.
+echo To do so, right click on this script and select 'Run as administrator'.
+goto MASend
+)
+
+::========================================================================================================================================
+
+setlocal DisableDelayedExpansion
+
+::  Check desktop location
+
+set _desktop_=
+for /f "skip=2 tokens=2*" %%a in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop') do call set "_desktop_=%%b"
+if not defined _desktop_ for /f "delims=" %%a in ('%psc% "& {write-host $([Environment]::GetFolderPath('Desktop'))}"') do call set "_desktop_=%%a"
+
+set "_pdesk=%_desktop_:'=''%"
+setlocal EnableDelayedExpansion
+
+::========================================================================================================================================
+
+cls
 
 SETLOCAL EnableDelayedExpansion
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
