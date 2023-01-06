c5d3752f98ec115860bbeee746acc874caf9e491
/codsec <44205014+codsec@users.noreply.github.com>
2023-01-06 10:11:23 -0300

    Update install.cmd

--- MAS/All-In-One-Version/install.cmd
+++ MAS/All-In-One-Version/install.cmd
@@ -1,6 +1,13 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
+:: Colors
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+
+
 @echo off
 cls
 
